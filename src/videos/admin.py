e455f0e8b9a0335b92c0c946a08a7106146a5cc3
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-11-15 22:02:30 +0500

    admin panel change, 3-qism tugadi

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import VideoAllProxy, VideoPublishedProxy
 
-# Register your models here.
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id', 'is_published']
+    search_fields = ['video_id']
+    list_filter = ['active', 'video_id']
+
+    class Meta:
+        model = VideoAllProxy
+    # def published(self, obj, *args, **kwargs):
+    #     return obj.active
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['video_id']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
