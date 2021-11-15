2bcd004e8db773ea7753842b391bc9e7d621f41c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-11-15 20:47:14 +0500

    video model test qilindi, 2-qism tugadi

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Video
 # Create your tests here.
+
+class VideoMOdelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='This is my title')
+
+    def test_valid_title(self):
+        title = 'This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
+    def test_created_count(self):
+        title = 'This is my title'
+        qs = Video.objects.all()
+        self.assertEqual(qs.count(),1)
