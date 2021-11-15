edb3a4610d7f7580abd82352efbfef065ec8aa0c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-11-15 22:01:52 +0500

    qoshimcha modellar yaratildi

--- src/videos/models.py
+++ src/videos/models.py
@@ -6,6 +6,23 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=220)
+    active = models.BooleanField(default=True)
     # timestamp
     # update
     # state
+
+    @property
+    def is_published(self):
+        return self.active
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = "All video"
+        verbose_name_plural = "All videos"
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = "Published video"
+        verbose_name_plural = "Published videos"
