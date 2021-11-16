e0a248e685c1a41471b25b0ff7b082800a23bfdf
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-11-16 19:34:32 +0500

    modellar test qilindi

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from .models import Video
+from django.utils import timezone
+
 # Create your tests here.
 
 class VideoMOdelTestCase(TestCase):
     def setUp(self):
         Video.objects.create(title='This is my title')
+        Video.objects.create(title='This is my title', state=Video.VideoStateOptions.PUBLISH)
 
     def test_valid_title(self):
         title = 'This is my title'
@@ -14,4 +17,14 @@ class VideoMOdelTestCase(TestCase):
     def test_created_count(self):
         title = 'This is my title'
         qs = Video.objects.all()
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
+
+    def test_draft_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
+        self.assertEqual(qs.count(), 1)
+
+    def test_publish_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.PUBLISH)
+        now = timezone.now()
+        published_qs = Video.objects.filter(publish_timestamp__lte=now)
+        self.assertTrue(published_qs.exists())
