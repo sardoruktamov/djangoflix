41dd3c0d9baf14451b4a265ea2fd0bede072e156
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-12-04 21:27:39 +0500

    modelga qo`shimcha qoshildi

--- src/videos/models.py
+++ src/videos/models.py
@@ -35,6 +35,8 @@ class Video(models.Model):
     def is_published(self):
         return self.active
 
+    def get_playlist_ids(self):
+        return list(self.playlist_set.all().values_list('id', flat=True))
 
 class VideoAllProxy(Video):
     class Meta:
