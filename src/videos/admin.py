1e383654d6006857f6be49cc109b37998415ebd7
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-12-04 21:28:11 +0500

    admin change

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -3,10 +3,10 @@ from .models import VideoAllProxy, VideoPublishedProxy
 
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'state', 'video_id', 'is_published']
+    list_display = ['title', 'state', 'video_id', 'is_published', 'get_playlist_ids']
     search_fields = ['video_id']
     list_filter = ['active', 'state']
-    readonly_fields = ['id', 'publish_timestamp']
+    readonly_fields = ['id', 'publish_timestamp', 'get_playlist_ids']
 
     class Meta:
         model = VideoAllProxy
